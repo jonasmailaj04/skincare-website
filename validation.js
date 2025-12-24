a28543d6689f4313f9902f51e2c730b975a5812c
Jona Smailaj <jona.smailaj04@gmail.com>
2025-12-24 08:53:29 +0100

    Update validation.js
    sign up

--- validation.js
+++ validation.js
@@ -23,3 +23,38 @@ function validateStrongPassword(password) {
     
     return errors
 }
+
+function getSignupFormErrors(firstname, email, password, repeatPassword, firstname_input, email_input, password_input, repeat_password_input) {
+    let errors = []
+
+    if( firstname === '' || firstname == null){
+        errors.push('Firstname is required')
+        if (firstname_input) firstname_input.parentElement.classList.add('incorrect')
+    }
+
+    if( email === '' || email == null){
+        errors.push('email is required')
+        if (email_input) email_input.parentElement.classList.add('incorrect')
+    }
+
+    if( password === '' || password == null){
+        errors.push('password is required')
+        if (password_input) password_input.parentElement.classList.add('incorrect')
+    } else {
+        const passwordErrors = validateStrongPassword(password)
+        if (passwordErrors.length > 0) {
+            errors.push(...passwordErrors)
+            if (password_input) password_input.parentElement.classList.add('incorrect')
+        }
+    }
+
+    if(password !== repeatPassword){
+        errors.push('Password does not match repeated password')
+        if (password_input) password_input.parentElement.classList.add('incorrect')
+        if(repeat_password_input) {
+            repeat_password_input.parentElement.classList.add('incorrect')
+        }
+    }
+
+    return errors;
+}
